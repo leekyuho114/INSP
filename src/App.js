2ba2ca0272a7c31849a89952178bea04a04aad1b
leekyuho <rbgh0114@naver.com>
2023-07-13 21:26:05 +0900

    navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
+import {Button, Container, Form, Nav, Navbar, NavDropdown, Offcanvas} from 'react-bootstrap';
 
 function App() {
+  let navigate = useNavigate();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {[false].map((expand) => (
+        <Navbar  key={expand} expand={expand} className="color-nav mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#">Project</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+                  <Nav.Link href="#action2">Link</Nav.Link>
+                  <NavDropdown
+                    title="Dropdown"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Something else here
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+      <Routes>
+        <Route path="/" element={<></>}/>
+        <Route path="/detail/:id" element={<div></div>}/>
+        <Route path="/about" element={<div></div>}/>
+        <Route path="/event" element={<div></div>}>
+          <Route path="one" element={<div></div>}/>
+          <Route path="two" element={<div></div>}/>
+        </Route>
+        <Route path="*" element={<div>404 ERROR</div>}/>
+      </Routes>
+    </>
   );
 }
 
